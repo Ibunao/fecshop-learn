496f30944d9b508e7497dfaca41b226b65b4567b
unknown <dingran@uki.com>
2018-09-03 16:16:07 +0800

    初始配置

--- common/config/fecshop_local_services/Image.php
+++ common/config/fecshop_local_services/Image.php
@@ -1,30 +1,28 @@
 <?php
 /**
  * FecShop file.
- *
  * @link http://www.fecshop.com/
- *
  * @copyright Copyright (c) 2016 FecShop Software LLC
  * @license http://www.fecshop.com/license/
  */
 return [
     'image' => [
-        'appbase'    => [
+        'appbase'   => [
             'appfront' => [
-                'basedir'    => '@appimage/appfront',
-                'basedomain' => '//img.appfront.fancyecommerce.com',
+                'basedir' => '@appimage/appfront',
+                'basedomain' => '//img3.fecshoptest.com',
             ],
             'apphtml5' => [
-                'basedir'    => '@appimage/apphtml5',
-                'basedomain' => '//img.apphtml5.fancyecommerce.com',
+                'basedir' => '@appimage/apphtml5',
+                'basedomain' => '//img2.fecshoptest.com',
             ],
-            //'appadmin' => [
-            //	'basedir' => '@appimage/appadmin',
-            //	'basedomain' => '//img.appadmin.fancyecommerce.com',
-            //],
+            'appadmin' => [
+                'basedir' => '@appimage/appadmin',
+                'basedomain' => '//img2.fecshoptest.com',
+            ],
             'common' => [
-                'basedir'    => '@appimage/common',
-                'basedomain' => '//img.fancyecommerce.com',
+                'basedir' => '@appimage/common',
+                'basedomain' => '//img.fecshoptest.com',
             ],
         ],
     ],
